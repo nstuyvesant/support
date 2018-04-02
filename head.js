e1d22489b00c6314edae1d3bedda8d65167cfca1
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-04-01 22:21:18 -0400

    fix

--- head.js
+++ head.js
@@ -1,24 +1,24 @@
 // If website isn't https://support.perfecto.io, redirect
-if(location.protocol != 'https' || window.location.hostname != 'support.perfecto.io') {
+if (location.protocol != 'https' || window.location.hostname != 'support.perfecto.io') {
     alert('redirect');
     window.location.replace(`https://support.perfecto.io/${location.search}`);
 }
 
-// Salesforce Live Agent
-liveagent.init('https://d.la1-c1-lon.salesforceliveagent.com/chat', '572D000000001yp', '00D200000001UUG');
-if (!window._laq) {
-    window._laq = [];
-}
-window._laq.push(function(){
-    liveagent.showWhenOnline('573D0000000028q', document.getElementById('liveagent_button_online_573D0000000028q'));
-    liveagent.showWhenOffline('573D0000000028q', document.getElementById('liveagent_button_offline_573D0000000028q'));
-});
+// // Salesforce Live Agent
+// liveagent.init('https://d.la1-c1-lon.salesforceliveagent.com/chat', '572D000000001yp', '00D200000001UUG');
+// if (!window._laq) {
+//     window._laq = [];
+// }
+// window._laq.push(function() {
+//     liveagent.showWhenOnline('573D0000000028q', document.getElementById('liveagent_button_online_573D0000000028q'));
+//     liveagent.showWhenOffline('573D0000000028q', document.getElementById('liveagent_button_offline_573D0000000028q'));
+// });
 
-// Global site tag (gtag.js) - Google Analytics
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-2078617-3');
+// // Global site tag (gtag.js) - Google Analytics
+// window.dataLayer = window.dataLayer || [];
+// function gtag(){dataLayer.push(arguments);}
+// gtag('js', new Date());
+// gtag('config', 'UA-2078617-3');
 
 // // Marketo Munchkin
 // (function() {
