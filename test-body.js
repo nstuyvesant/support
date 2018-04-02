1c8d7e23df2f0fbc0e788f9374a66fe7cdc964ab
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-04-01 21:41:09 -0400

    test(json): push

--- test-body.js
+++ test-body.js
@@ -9,8 +9,21 @@ $('#issueTypes').on('shown.bs.tab', function(e) {
     var selectedTabName = $(e.target).attr('aria-controls');
     $('#issueTypeSelected').text(selectedTabName);
     $('.typeahead').typeahead({
+        // TODO: load the one appropriate for selectedTabName
         source: caseTree.types[0].topics[0].subtopics
     });
+    $('#searchResults').empty(); // Clear the card of past articles
+    $('#searchResults').append('<br/><h2>Perhaps one of these can help?</h2');
+    var searchResults = [];
+    var article = '';
+    $.each(caseTree.types[0].topics[0].articles, function(index, value) {
+        value.url = `http://developers.perfectomobile.com/pages/viewpage.action?pageId=${value.id}`;
+        article = `<article><a href="${value.url}" target="_blank">${value.title}</a><p class="text-muted">${value.synopsis}</p></article>`;
+        $('#searchResults').append(article);
+        searchResults.push(article);
+    });
+    $('#searchResults').append("<h2>Didn't help? Please continue below to open a case...</h2");
+    $('body').css('cursor','default');
 });
 
 // DOM loaded
