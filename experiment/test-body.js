f28bf80181b9c5bb78552478e2500f364e5fdd96
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-04-28 17:53:34 -0400

    chore(state): reset when type chosen

--- experiment/test-body.js
+++ experiment/test-body.js
@@ -2,15 +2,20 @@
 var selectedType;
 var selectedTopic;
 
-// Set global type based on tab selection
+// Type chosen and tab displayed. Set global variable and reset state
 $('#typeTabs').on('shown.bs.tab', function(e) {
     selectedType = $(e.target).attr('aria-controls');
-    if(selectedType == 'Suggestion') {
-        $('#articleContent').collapse();
-    }
+
+    // Hide Contact Support (if displayed) until a Topic is re-selected
+    $('#contactSupport').removeAttr('style');
+
+    // Reset the Topic selection and hide any visible articles
+    selectedTopic = '';
+    $('.nav-pills > li > a.nav-link').removeClass('active show');
+    $('#articleContent > .tab-pane').removeClass('active show');
 });
 
-// Set global topic based on tab selection
+// Topic chosen and tab displayed. Set global variable.
 $('ul.nav-pills').on('shown.bs.tab', function(e) {
     selectedTopic = $(e.target).attr('aria-controls');
     $('#topic').val(selectedTopic);
@@ -22,11 +27,6 @@ $('.with-tooltips').on('shown.bs.collapse', function() {
     $('[data-toggle="tooltip"]').tooltip({ // Turn on tool tips for the now-visible form
         container: 'body'
     });
-});
-
-$('#suggestionType').on('click', function(e) {
-    $('.tab-pane').removeClass('active show');
-    $('#contactSupport').removeAttr('style');
 });
 
 // DOM loaded
