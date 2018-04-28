6408a0d101c5af3fd1df7feacbf447c08967a72c
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-04-28 13:06:23 -0400

    chore(tooltips): enabled

--- experiment/test-body.js
+++ experiment/test-body.js
@@ -1,33 +1,40 @@
-var caseTree = {};
+// var caseTree = {};
 
-$.getJSON('test-hierarchy.json', function(data) {
-    caseTree = data;
+// $.getJSON('test-hierarchy.json', function(data) {
+//     caseTree = data;
+// });
+
+// // Handle Support Case is displayed
+$('#supportCase').on('shown.bs.collapse', function() {
+    $('[data-toggle="tooltip"]').tooltip({ // Turn on tool tips for the now-visible form
+        container: 'body'
+    });
 });
 
-// Handle click on tabs
-$('#issueTypes').on('shown.bs.tab', function(e) {
-    //console.log(e);
-    var selectedTabName = $(e.target).attr('aria-controls');
-    $('#issueTypeSelected').text(selectedTabName);
-    $('.typeahead').typeahead({
-        // TODO: load the one appropriate for selectedTabName
-        source: caseTree.types[0].topics[0].subtopics
-    });
-    $('#searchResults').empty(); // Clear the card of past articles
-    $('#searchResults').append('<br/><h2>Maybe one of these articles can help...</h2');
-    var searchResults = [];
-    var article = '';
-    $.each(caseTree.types[0].topics[0].articles, function(index, value) {
-        value.url = `http://developers.perfectomobile.com/pages/viewpage.action?pageId=${value.id}`;
-        article = `<article><a href="${value.url}" target="_blank">${value.title}</a><p class="text-muted">${value.synopsis}</p></article>`;
-        $('#searchResults').append(article);
-        searchResults.push(article);
-    });
-    $('#didNotHelp').show();
-    $('body').css('cursor','default');
-});
+// // Handle click on tabs
+// $('#issueTypes').on('shown.bs.tab', function(e) {
+//     //console.log(e);
+//     var selectedTabName = $(e.target).attr('aria-controls');
+//     $('#issueTypeSelected').text(selectedTabName);
+//     $('.typeahead').typeahead({
+//         // TODO: load the one appropriate for selectedTabName
+//         source: caseTree.types[0].topics[0].subtopics
+//     });
+//     $('#searchResults').empty(); // Clear the card of past articles
+//     $('#searchResults').append('<br/><h2>Maybe one of these articles can help...</h2');
+//     var searchResults = [];
+//     var article = '';
+//     $.each(caseTree.types[0].topics[0].articles, function(index, value) {
+//         value.url = `http://developers.perfectomobile.com/pages/viewpage.action?pageId=${value.id}`;
+//         article = `<article><a href="${value.url}" target="_blank">${value.title}</a><p class="text-muted">${value.synopsis}</p></article>`;
+//         $('#searchResults').append(article);
+//         searchResults.push(article);
+//     });
+//     $('#didNotHelp').show();
+//     $('body').css('cursor','default');
+// });
 
 // DOM loaded
 $(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip(); // Turn on tool tips
+    
 });
