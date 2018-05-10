b5f6c432a14d2fcae343a65960a24af9fd3fba18
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-05-10 19:11:17 -0400

    chore(php): update

--- hello.php
+++ hello.php
@@ -1,5 +1,4 @@
 <?php
-include("/php/geoip.inc");
 header( "HTTP/1.1 200 OK" );
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
