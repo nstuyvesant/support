94a505a74b8694bd915b2f1d36cc650d750a5879
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-05-10 18:06:53 -0400

    chore(php): test

--- hello.php
+++ hello.php
@@ -1,4 +1,7 @@
 <?php
-  phpinfo();
-  //echo 'Your IP address is: ' + $_SERVER['REMOTE_ADDR'];
+  //phpinfo();
+  echo('Your IP address is: ' + $_SERVER['REMOTE_ADDR']);
+  echo('Seems like your real IP is: ' + $_SERVER['HTTP_CF_CONNECTING_IP']);
+  echo('Forwarding address is:' + $_SERVER['HTTP_X_FORWARDED_FOR']);
+  echo('Country is: ' + $_SERVER['HTTP_CF_IPCOUNTRY']);
 ?>
