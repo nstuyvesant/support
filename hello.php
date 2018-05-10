b3bf51849be9242759c83555964fdee6b4d6ae20
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-05-10 18:15:16 -0400

    chore(php): json test

--- hello.php
+++ hello.php
@@ -1,8 +1,14 @@
 <?php
   //phpinfo();
   //72.95.128.78
+  header("Access-Control-Allow-Origin: *");
+  $netInfo->country = $_SERVER['HTTP_CF_IPCOUNTRY'];
+  $netInfo->ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+  echo json_encode($netInfo);
+/*
   echo "Your IP address is: " . $_SERVER['REMOTE_ADDR'];
   echo "Seems like your real IP is: " . $_SERVER['HTTP_CF_CONNECTING_IP'];
   echo "Forwarding address is:" . $_SERVER['HTTP_X_FORWARDED_FOR'];
   echo "Country is: " . $_SERVER['HTTP_CF_IPCOUNTRY'];
+*/
 ?>
