dbed913574bde7f68943896c233415d31f8601d3
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-05-10 17:58:21 -0400

    chore(php): info

--- hello.php
+++ hello.php
@@ -1,8 +1,4 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
+<?php
+  phpinfo();
+  //echo 'Your IP address is: ' + $_SERVER['REMOTE_ADDR'];
+?>
