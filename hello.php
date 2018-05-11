d3c13f9859febff30b7790566b0c23e0ca65d716
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-05-10 22:52:28 -0400

    php

--- hello.php
+++ hello.php
@@ -8,26 +8,6 @@ header("Access-Control-Allow-Origin: *");
 
 // Function to get the client IP address
 function get_client_ip() {
-  $ipaddress = "";
-  if (getenv("HTTP_CLIENT_IP"))
-      $ipaddress = getenv("HTTP_CLIENT_IP");
-  else if(getenv("HTTP_X_FORWARDED_FOR"))
-      $ipaddress = getenv("HTTP_X_FORWARDED_FOR");
-  else if(getenv("HTTP_X_FORWARDED"))
-      $ipaddress = getenv("HTTP_X_FORWARDED");
-  else if(getenv("HTTP_FORWARDED_FOR"))
-      $ipaddress = getenv("HTTP_FORWARDED_FOR");
-  else if(getenv("HTTP_FORWARDED"))
-      $ipaddress = getenv("HTTP_FORWARDED");
-  else if(getenv("REMOTE_ADDR"))
-      $ipaddress = getenv("REMOTE_ADDR");
-  else
-      $ipaddress = "UNKNOWN";
-
-  return $ipaddress;
-}
-
-function get_true_ip() {
     $ip_keys = array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR');
     foreach ($ip_keys as $key) {
         if (array_key_exists($key, $_SERVER) === true) {
@@ -47,8 +27,7 @@ function get_true_ip() {
  * Ensures an ip address is both a valid IP and does not fall within
  * a private network range.
  */
-function validate_ip($ip)
-{
+function validate_ip($ip) {
     if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false) {
         return false;
     }
@@ -91,14 +70,17 @@ $ip = get_client_ip(); //72.95.128.78
 $netInfo->ip = $ip;
 $netInfo->remoteAddr = $_SERVER["REMOTE_ADDR"];
 $netInfo->forwardedFor = $_SERVER["HTTP_X_FORWARDED_FOR"];
-$netInfo->trueIp = get_true_ip();
 $netInfo->proxy = get_proxy_info($ip);
 
-$arr = json_decode(file_get_contents("http://ip-api.com/json//$ip"), JSON_UNESCAPED_SLASHES);
+$arr = json_decode(file_get_contents("http://ip-api.com/json//$ip"), true);
 $netInfo->city = $arr["city"];
 $netInfo->region = $arr["region"];
 $netInfo->country = $arr["countryCode"];
 $netInfo->isp = $arr["isp"];
 $netInfo->timezone = $arr["timezone"];
-echo json_encode($netInfo, JSON_UNESCAPED_SLASHES);
+
+$arr = json_decode(file_get_contents("http://proxycheck.io/v2/$ip?key=u188a3-680b4p-38550a-06w364&vpn=1"), true);
+echo json_encode($arr[$ip], JSON_UNESCAPED_SLASHES);
+
+//echo json_encode($netInfo, JSON_UNESCAPED_SLASHES);
 ?>
