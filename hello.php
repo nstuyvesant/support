3d92c68bc087513dd003efb351a5bdedb91f75e1
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-05-10 20:11:51 -0400

    php

--- hello.php
+++ hello.php
@@ -78,7 +78,7 @@ function get_isp() {
 
 $ip = get_client_ip();
 $netInfo->ip = $ip;
-$netInfo->proxy = get_proxy_info($ip);
+//$netInfo->proxy = get_proxy_info($ip);
 $json = file_get_contents("http://api.ipstack.com/$ip?access_key=7c21bd608096d60dbe33bcd139a3e0af");
 $netInfo->city = $json['city'];
 $netInfo->region = $json['region_code'];
