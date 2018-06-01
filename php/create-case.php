3cb9bdc70afebab62fd6044542939567b3037280
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-06-01 15:05:31 -0400

    chore(create-case): add fields

--- php/create-case.php
+++ php/create-case.php
@@ -1,14 +1,29 @@
 <?php
 # TODO: Replace hard-coded values with array of name/value pairs from POST - $('#requestForm').serializeArray()
-$subject = 'Test subject';
-$description = 'This is only a test';
-$Priority = 'Low';
-$appUrl = 'cloud.perfectomobile.com';
-$suppliedName = 'Firstname Lastname';
-$suppliedEmail = 'nul@bitbucket.com';
-$SuppliedPhone = '+111111111111';
-$filename = 'file.txt';
-$path = 'ATTACHMENT.txt';
+$type = 'Non-issue';
+$topic = 'Non-issue: Testing';
+$origin = 'Web';
+$recordType = 'Support';
+$timezone = -5;
+$company = 'Acme, Inc.';
+$mcmVersion = '18.6';
+$hssVersion = '18.6';
+$location = 'NA-US-BOS';
+$cradleId = 'AKFOKEF303';
+$deviceId = 'DAKD39399958855';
+$status = 'Closed - Canceled';
+$model = 'iPhone-8';
+$os = 'iOS';
+$version = '11.1';
+$subject = 'Test subject'; # $_POST["subject"]
+$description = 'This is only a test'; # $_POST["description"]
+$priority = 'Low'; #_POST["priority"]
+$fqdn = 'cloud.perfectomobile.com'; # $_POST["appUrl"]
+$suppliedName = 'Firstname Lastname'; # $_POST["suppliedName"]
+$suppliedEmail = 'nul@bitbucket.com'; # $_POST["suppledEmail"]
+$suppliedPhone = '+111111111111'; # $_POST["suppliedPhone"]
+$filename = 'file.txt'; # What to call the attachment in Salesforce
+$path = 'ATTACHMENT.txt'; # The local path to the file being uploaded
 
 # Retrieve Salesforce connection info
 $SfConfig = parse_ini_file('conf.ini');
@@ -20,17 +35,33 @@ require_once('soapclient/SforceEnterpriseClient.php');
 # Instantiate the Salesforce connection
 $mySforceConnection = new SforceEnterpriseClient();
 $mySoapClient = $mySforceConnection->createConnection('soapclient/perfectomobile.xml');
-$mylogin = $mySforceConnection->login(USERNAME,PASSWORD.SECURITY_TOKEN);
+$mylogin = $mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
 
 # Set properties of Salesforce Case object
 $sObject = new stdclass();
+$sObject->RecordType = $recordType;
+$sObject->Status = $status;
+$sObject->Origin = $origin;
+$sObject->Type = $type;
+$sObject->Case_Reason__c = $topic;
+$sObject->Priority = $priority;
 $sObject->Subject = $subject;
 $sObject->Description = $description;
-$sObject->Priority = $Priority; 
-$sObject->AppURL__c = $appUrl;
+$sObject->AppURL__c = $fqdn;
 $sObject->SuppliedName = $suppliedName;
 $sObject->SuppliedEmail = $suppliedEmail;
-$sObject->SuppliedPhone = $SuppliedPhone; 
+$sObject->SuppliedPhone = $suppliedPhone;
+$sObject->SuppliedCompany = $company;
+$sObject->Customer_Time_Zone__c = $timezone;
+$sObject->MCM_Version__c = $mcmVersion;
+$sObject->HSS_Version__c = $hssVersion;
+$sObject->Location__c = $location;
+$sObject->Cradle__c = $cradleId;
+$sObject->Device_ID__c = $deviceId;
+$sObject->Device_ID__c = $deviceId;
+$sObject->Model__c = $model;
+$sObject->OS__c = $os;
+$sObject->Version__c = $version;
 
 # Create case
 try {
