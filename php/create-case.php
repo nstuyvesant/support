046272b0376d3d645761c6b61a1da6b211336ad3
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-06-01 16:09:59 -0400

    chore(create-case): update

--- php/create-case.php
+++ php/create-case.php
@@ -10,7 +10,6 @@ $hssVersion = '18.6';
 $location = 'NA-US-BOS';
 $cradleId = 'BOS-E8-4-1/VIRTUAL/02';
 $deviceId = '2A56E99775698D2F1BABD7C1F9D57CA06AF0C7F6';
-$status = 'Closed - Canceled';
 $model = 'iPhone-8';
 $os = 'iOS';
 $version = '11.1';
@@ -32,64 +31,58 @@ define(PASSWORD, $SfConfig['password']);
 require_once('soapclient/SforceEnterpriseClient.php');
 
 # Instantiate the Salesforce connection
-$mySforceConnection = new SforceEnterpriseClient();
-$mySoapClient = $mySforceConnection->createConnection('soapclient/perfectomobile.xml');
-$mylogin = $mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
+$connection = new SforceEnterpriseClient();
+$client = $connection->createConnection('soapclient/perfectomobile.xml');
+$login = $connection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
 
 # Set properties of Salesforce Case object
-$sObject = new stdclass();
-$sObject->Status = $status; # PROBLEM
-$sObject->Origin = $origin;
-$sObject->Type = $type;
-$sObject->Case_Reason__c = $topic;
-$sObject->Priority = $priority;
-$sObject->Subject = $subject;
-$sObject->Description = $description;
-$sObject->AppURL__c = $fqdn;
-$sObject->SuppliedName = $suppliedName;
-$sObject->SuppliedEmail = $suppliedEmail;
-$sObject->SuppliedPhone = $suppliedPhone;
-$sObject->SuppliedCompany = $company;
-$sObject->Customer_Time_Zone__c = $timezone;
-$sObject->MCM_Version__c = $mcmVersion;
-$sObject->HSS_Version__c = $hssVersion;
-$sObject->Location__c = $location;
-$sObject->Cradle__c = $cradleId;
-$sObject->Device_ID__c = $deviceId;
-$sObject->Device_ID__c = $deviceId;
-$sObject->Model__c = $model;
-$sObject->OS__c = $os;
-$sObject->Version__c = $version;
+$case = new stdClass();
+$case->Origin = $origin;
+$case->Type = $type;
+$case->Case_Reason__c = $topic;
+$case->Priority = $priority;
+$case->Subject = $subject;
+$case->Description = $description;
+$case->AppURL__c = $fqdn;
+$case->SuppliedName = $suppliedName;
+$case->SuppliedEmail = $suppliedEmail;
+$case->SuppliedPhone = $suppliedPhone;
+$case->SuppliedCompany = $company;
+$case->Customer_Time_Zone__c = $timezone;
+$case->MCM_Version__c = $mcmVersion;
+$case->HSS_Version__c = $hssVersion;
+$case->Location__c = $location;
+$case->Cradle__c = $cradleId;
+$case->Device_ID__c = $deviceId;
+$case->Device_ID__c = $deviceId;
+$case->Model__c = $model;
+$case->OS__c = $os;
+$case->Version__c = $version;
 
-# Create case (though it's setup for mass creates via array)
+# Create one case and return case number (though setup for mass creates via array)
 try {
-  $caseResponse = $mySforceConnection->create(array($sObject), 'Case');
-  foreach ($caseResponse as $record) $parent = $record->id;
-  $json['case'] = $caseResponse;
+  $caseResponse = $connection->create(array($case), 'Case');
+  foreach ($caseResponse as $record) $parent = $record->id; # There's only one
 
   # Upload attachment if one exists and case was created
   if (file_exists($path)) {
     try {
-      $sfObj = new stdClass();
       $data = file_get_contents($path);
-      $sfObj->Body = base64_encode($data);
-      $sfObj->Name = $filename;
-      $sfObj->ParentId = $parent;
-      
-      $attachmentResponse = $mySforceConnection->create(array($sfObj), 'Attachment');
-      $json['attachment'] = $attachmentResponse;
+      $attachment = new stdClass();
+      $attachment->Body = base64_encode($data);
+      $attachment->Name = $filename;
+      $attachment->ParentId = $parent;
+      $attachmentResponse = $connection->create(array($attachment), 'Attachment');
     } catch (Exception $attachmentError) {
       # Failed to attach file
-      # $json['attachment'] = NULL;
       echo json_encode($attachmentError);
     }
   }
   # Respond with a JSON object representing the case (though we only need the Case Number)
   echo json_encode($caseResponse);
-  #print_r($json);
+  # print_r($caseResponse); # for debugging
 } catch (Exception $caseError) {
   # Create case failed 
-  # $json['case'] = NULL;
   echo json_encode($caseError);
 }
  ?>
