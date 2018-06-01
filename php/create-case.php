693e57942cbe5208751816635c7ad40b4e5e7d0c
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-06-01 09:44:24 -0400

    chore(create-case): add Ran's PHP code

--- php/create-case.php
+++ php/create-case.php
@@ -1,34 +1,63 @@
 <?php
-# Handle HTTP POST of case fields from input form
+# TODO: Replace hard-coded values with array of name/value pairs from POST - $('#requestForm').serializeArray()
+$subject = 'Test subject';
+$description = 'This is only a test';
+$Priority = 'Low';
+$appUrl = 'cloud.perfectomobile.com';
+$suppliedName = 'Firstname Lastname';
+$suppliedEmail = 'nul@bitbucket.com';
+$SuppliedPhone = '+111111111111';
+$filename = 'file.txt';
+$path = 'ATTACHMENT.txt';
 
-# Connect to Salesforce
-$SfConfig = parse_ini_file("conf.ini");
+# Retrieve Salesforce connection info
+$SfConfig = parse_ini_file('conf.ini');
 define(SECURITY_TOKEN, $SfConfig['token']);
 define(USERNAME, $SfConfig['user']);
 define(PASSWORD, $SfConfig['password']);
 require_once('soapclient/SforceEnterpriseClient.php');
+
+# Instantiate the Salesforce connection
 $mySforceConnection = new SforceEnterpriseClient();
-$mySoapClient = $mySforceConnection->createConnection("soapclient/perfectomobile.xml");
+$mySoapClient = $mySforceConnection->createConnection('soapclient/perfectomobile.xml');
 $mylogin = $mySforceConnection->login(USERNAME,PASSWORD.SECURITY_TOKEN);
 
-# Set Case fields
-# TODO: Replace with POSTed values
+# Set properties of Salesforce Case object
 $sObject = new stdclass();
-$sObject->Subject = "This is a test";
-$sObject->Description = "Please disregard this case.";
-$sObject->Priority = "Low";
-$sObject->AppURL__c = "demo.perfectomobile.com";
-$sObject->SuppliedName = "Firstname Lastname";
-$sObject->SuppliedEmail = "demo@perfectomobile.com";
-$sObject->SuppliedPhone = "+1-000-000-0000";
+$sObject->Subject = $subject;
+$sObject->Description = $description;
+$sObject->Priority = $Priority; 
+$sObject->AppURL__c = $appUrl;
+$sObject->SuppliedName = $suppliedName;
+$sObject->SuppliedEmail = $suppliedEmail;
+$sObject->SuppliedPhone = $SuppliedPhone; 
 
+# Create case
 try {
   $response = $mySforceConnection->create(array($sObject), 'Case');
-  print_r($response);
-  exit;
+  foreach ($response as $record) $parent = $record->id;
+  $json['case'] = $response;
+
+  # Upload attachment if one exists and case was created
+  if (file_exists($path)) {
+    try {
+      $sfObj = new stdClass();
+      $data = file_get_contents($path);
+      $sfObj->Body = base64_encode($data);
+      $sfObj->Name = $filename;
+      $sfObj->ParentId = $parent;
+      
+      $response = $mySforceConnection->create(array($sfObj), 'Attachment');
+      $json['attachment'] = $response;
+    } catch (Exception $e) {
+      # Failed to attach file
+      # $json['attachment'] = NULL;
+    }
+  }
+  $myJSON = json_encode($json);
+  print_r($json);
 } catch (Exception $e) {
-  echo $mySforceConnection->getLastRequest();
-  echo $e->faultstring;
-  exit;
+  # Create case failed 
+  # $json['case'] = NULL;
 }
-?>
+ ?>
