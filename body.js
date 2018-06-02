04c3afbf98385e2fa4422eed3e579337cd20d1ce
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-06-02 11:22:42 -0400

    refactor(qs): remove unnecessary escapes

--- body.js
+++ body.js
@@ -22,7 +22,7 @@ function recaptchaCallback () {
 
 // Extract querystring values
 function qs (key) {
-  key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, '\\$&') // escape RegEx meta chars
+  key = key.replace(/[*+?^$.[\]{}()|\\/]/g, '\\$&') // escape RegEx meta chars
   let match = location.search.match(new RegExp('[?&]' + key + '=([^&]+)(&|$)'))
   return match && decodeURIComponent(match[1].replace(/\+/g, ' '))
 }
