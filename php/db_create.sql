e811b27fcce7f4f4acc0189849dce57895e357f6
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-06-08 12:42:37 -0400

    chore(db_create): add

---
+++ php/db_create.sql
@@ -0,0 +1,408 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.4
+-- Dumped by pg_dump version 10.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Database: vr
+
+-- DROP DATABASE vr;
+
+CREATE DATABASE vr
+    WITH 
+    OWNER = nates
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE vr
+    IS 'Value Realization';
+    
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: clouds; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.clouds (
+    id uuid NOT NULL,
+    fqdn character varying(255) NOT NULL,
+    email_recipients character varying(4000)
+);
+
+
+--
+-- Name: COLUMN clouds.fqdn; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.clouds.fqdn IS 'Fully-qualified domain name of the Perfecto cloud';
+
+
+--
+-- Name: COLUMN clouds.email_recipients; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.clouds.email_recipients IS 'Comma-separated list of email recipients for the report (typically Champion, VRC, BB, and DAs)';
+
+
+--
+-- Name: devices; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.devices (
+    id uuid NOT NULL,
+    snapshot_id uuid NOT NULL,
+    rank smallint DEFAULT 1 NOT NULL,
+    model character varying(255) NOT NULL,
+    os character varying(255) NOT NULL,
+    device_id character varying(255) NOT NULL,
+    errors_last7d bigint NOT NULL
+);
+
+
+--
+-- Name: COLUMN devices.snapshot_id; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.devices.snapshot_id IS 'Foreign key to snapshot record';
+
+
+--
+-- Name: COLUMN devices.rank; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.devices.rank IS 'Report ranking of the importance of the problematic device';
+
+
+--
+-- Name: COLUMN devices.model; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.devices.model IS 'Model of the device such as "iPhone X" (manufacturer not needed)';
+
+
+--
+-- Name: COLUMN devices.os; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.devices.os IS 'Name of operating system and version number such as "iOS 11.3"';
+
+
+--
+-- Name: COLUMN devices.device_id; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.devices.device_id IS 'The device ID such as the UUID of an Apple iOS device or the serial number of an Android device';
+
+
+--
+-- Name: COLUMN devices.errors_last7d; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.devices.errors_last7d IS 'The number of times the device has gone into error over the last 7 days';
+
+
+--
+-- Name: recommendations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.recommendations (
+    id uuid NOT NULL,
+    snapshot_id uuid NOT NULL,
+    rank smallint DEFAULT 1 NOT NULL,
+    recommendation character varying(2000) NOT NULL,
+    impact_percentage smallint DEFAULT 0 NOT NULL,
+    impact_message character varying(2000)
+);
+
+
+--
+-- Name: COLUMN recommendations.snapshot_id; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.recommendations.snapshot_id IS 'Foreign key to snapshot record';
+
+
+--
+-- Name: COLUMN recommendations.rank; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.recommendations.rank IS 'Report ranking of the importance of the recommendation';
+
+
+--
+-- Name: COLUMN recommendations.recommendation; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.recommendations.recommendation IS 'Specific recommendation such as "Replace top 5 failing devices" or "Remediate TransferMoney test"';
+
+
+--
+-- Name: COLUMN recommendations.impact_percentage; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.recommendations.impact_percentage IS 'Percentage of improvement to success rate if the recommendation is implemented (use 0 to 100 rather than decimal < 1)';
+
+
+--
+-- Name: COLUMN recommendations.impact_message; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.recommendations.impact_message IS 'For recommendations that do not have a clear impact such as "Ensure tests use Digitalzoom API" (impact should equal 0 for those)';
+
+
+--
+-- Name: snapshots; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.snapshots (
+    id uuid NOT NULL,
+    cloud_id uuid NOT NULL,
+    success_last24h smallint,
+    success_last7d smallint,
+    success_last30d smallint,
+    lab_issues bigint,
+    orchestration_issues bigint,
+    scripting_issues bigint
+);
+
+
+--
+-- Name: COLUMN snapshots.cloud_id; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.snapshots.cloud_id IS 'Foreign key to cloud';
+
+
+--
+-- Name: COLUMN snapshots.success_last24h; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.snapshots.success_last24h IS 'Success rate for last 24 hours expressed as an integer between 0 and 100 (not as decimal < 1)';
+
+
+--
+-- Name: COLUMN snapshots.success_last7d; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.snapshots.success_last7d IS 'Success percentage over the last 7 days expressed as an integer from 0 to 100 (not as decimal < 1)';
+
+
+--
+-- Name: COLUMN snapshots.success_last30d; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.snapshots.success_last30d IS 'Success percentage over the last 30 days expressed as an integer from 0 to 100 (not as decimal < 1)';
+
+
+--
+-- Name: COLUMN snapshots.lab_issues; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.snapshots.lab_issues IS 'The number of script failures due to device or browser issues in the lab over the last 24 hours';
+
+
+--
+-- Name: COLUMN snapshots.orchestration_issues; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.snapshots.orchestration_issues IS 'The number of script failures due to attempts to use the same device';
+
+
+--
+-- Name: COLUMN snapshots.scripting_issues; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.snapshots.scripting_issues IS 'The number of script failures due to a problem with the script or framework over the past 24 hours';
+
+
+--
+-- Name: tests; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.tests (
+    id uuid NOT NULL,
+    snapshot_id uuid NOT NULL,
+    rank smallint DEFAULT 1 NOT NULL,
+    test character varying(4000) NOT NULL,
+    age bigint NOT NULL,
+    failures_last7d bigint NOT NULL,
+    passes_last7d bigint NOT NULL
+);
+
+
+--
+-- Name: COLUMN tests.snapshot_id; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.tests.snapshot_id IS 'Foreign key to snapshot record';
+
+
+--
+-- Name: COLUMN tests.rank; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.tests.rank IS 'Report ranking of the importance of the problematic test';
+
+
+--
+-- Name: COLUMN tests.test; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.tests.test IS 'Name of the test having issues';
+
+
+--
+-- Name: COLUMN tests.age; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.tests.age IS 'How many days Digitalzoom has known about this test (used to select out tests that are newly created)';
+
+
+--
+-- Name: COLUMN tests.failures_last7d; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.tests.failures_last7d IS 'Number of failures of the test for the last 7 days';
+
+
+--
+-- Name: COLUMN tests.passes_last7d; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.tests.passes_last7d IS 'The number of times the test has passed over the last 7 days';
+
+
+--
+-- Name: clouds clouds_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.clouds
+    ADD CONSTRAINT clouds_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: devices devices_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.devices
+    ADD CONSTRAINT devices_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: recommendations recommendations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.recommendations
+    ADD CONSTRAINT recommendations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: snapshots snapshots_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.snapshots
+    ADD CONSTRAINT snapshots_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tests tests_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.tests
+    ADD CONSTRAINT tests_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: fki_clouds_fkey; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX fki_clouds_fkey ON public.snapshots USING btree (cloud_id);
+
+
+--
+-- Name: fki_snapshot_fkey; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX fki_snapshot_fkey ON public.devices USING btree (snapshot_id);
+
+
+--
+-- Name: fki_snapshots_fkey; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX fki_snapshots_fkey ON public.recommendations USING btree (snapshot_id);
+
+
+--
+-- Name: snapshots clouds_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.snapshots
+    ADD CONSTRAINT clouds_fkey FOREIGN KEY (cloud_id) REFERENCES public.clouds(id);
+
+
+--
+-- Name: devices snapshots_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.devices
+    ADD CONSTRAINT snapshots_fkey FOREIGN KEY (snapshot_id) REFERENCES public.snapshots(id);
+
+
+--
+-- Name: recommendations snapshots_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.recommendations
+    ADD CONSTRAINT snapshots_fkey FOREIGN KEY (snapshot_id) REFERENCES public.snapshots(id);
+
+
+--
+-- Name: tests snapshots_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.tests
+    ADD CONSTRAINT snapshots_fkey FOREIGN KEY (snapshot_id) REFERENCES public.snapshots(id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: -
+--
+
+GRANT ALL ON SCHEMA public TO postgres;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
